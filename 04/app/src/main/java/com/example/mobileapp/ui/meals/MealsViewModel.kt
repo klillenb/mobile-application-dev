0c777049a854e8bfff007a1c9455e65a174733f1
klillenb <kertlillenberk@gmail.com>
2023-03-19 15:35:42 +0200

    04

---
+++ 04/app/src/main/java/com/example/mobileapp/ui/meals/MealsViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.mobileapp.ui.meals
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MealsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is meals Fragment"
+    }
+    val text: LiveData<String> = _text
+
+}
