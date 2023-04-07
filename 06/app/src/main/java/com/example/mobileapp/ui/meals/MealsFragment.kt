a6dcf2c832b7f66bf6993f606923232123554998
klillenb <kertlillenberk@gmail.com>
2023-04-07 17:40:46 +0300

    cameraX example with photo capture & gallery

---
+++ 06/app/src/main/java/com/example/mobileapp/ui/meals/MealsFragment.kt
@@ -0,0 +1,41 @@
+package com.example.mobileapp.ui.meals
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.mobileapp.databinding.FragmentHomeBinding
+import com.example.mobileapp.databinding.FragmentMealsBinding
+
+class MealsFragment : Fragment() {
+
+    private var _binding: FragmentMealsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val mealsViewModel =
+            ViewModelProvider(this).get(MealsViewModel::class.java)
+
+        _binding = FragmentMealsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
