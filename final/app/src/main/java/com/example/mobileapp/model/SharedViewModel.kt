073c1ed2fbab9bc75367dbe128b7a184afef976a
knyrr <rynk@tlu.ee>
2023-05-09 16:08:27 +0300

    Migrate to shared ViewModel

--- final/app/src/main/java/com/example/mobileapp/model/SharedViewModel.kt
+++ final/app/src/main/java/com/example/mobileapp/model/SharedViewModel.kt
@@ -1,21 +1,33 @@
-package com.example.mobileapp.ui.home
+package com.example.mobileapp.model
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.mobileapp.dto.RecipeDto
+import com.example.mobileapp.repository.RecipeRepository
 import com.example.mobileapp.dto.FoodQuoteDto
 import com.example.mobileapp.network.NinjaApi
 import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    private val _quote = MutableLiveData<FoodQuoteDto>()
+//https://developer.android.com/codelabs/basic-android-kotlin-training-shared-viewmodel#3
 
+class SharedViewModel: ViewModel() {
+    private val _repository = RecipeRepository()
+    val recipeList: LiveData<List<RecipeDto>> = _repository.recipes
+
+    private val _quote = MutableLiveData<FoodQuoteDto>()
     val quote: LiveData<FoodQuoteDto> = _quote
 
     init {
         getFoodQuote()
+        getData()
     }
+
+    //recipes
+    private fun getData() { _repository.getRecipes() }
+
+    //quotes
     private fun getFoodQuote() {
         viewModelScope.launch {
             try {
@@ -32,4 +44,5 @@ class HomeViewModel : ViewModel() {
         headerMap["X-Api-Key"] = NinjaApi.apiKey
         return headerMap
     }
+
 }
