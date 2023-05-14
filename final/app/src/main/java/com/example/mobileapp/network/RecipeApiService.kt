1d24ecf192e2320980d4244cf59075e6b02fe3c3
klillenb <kertlillenberk@gmail.com>
2023-05-14 20:22:10 +0300

    API service documentation

--- final/app/src/main/java/com/example/mobileapp/network/RecipeApiService.kt
+++ final/app/src/main/java/com/example/mobileapp/network/RecipeApiService.kt
@@ -38,16 +38,34 @@ object RecipeApi {
 }
 
 interface RecipeApiService {
+    /**
+     * GET all recipes from DB
+     * @return Response object, use .code() method for HTTP status code, .body() for response data
+     */
     @GET("recipe")
     suspend fun getRecipes(): Response<List<RecipeDto>>
 
+    /**
+     * GET food related quote from server
+     * @return Response object, use .code() method for HTTP status code, .body() for response data
+     */
     @GET("quote")
     suspend fun getQuote(): Response<List<FoodQuoteDto>>
 
+    /**
+     * POST new recipe to DB
+     * @param recipeData the new recipe to be inserted
+     * @return Response object, use .code() and .body() methods for request result
+     */
     @Headers("Content-Type: application/json")
     @POST("recipe/add")
     suspend fun addRecipe(@Body recipeData: RecipeDto): Response<String>
 
+    /**
+     * DELETE recipe from DB
+     * @param recipeId id of recipe to remove
+     * @return Response object, use .code() and .body() methods for request result
+     */
     @DELETE("recipe/{id}")
     suspend fun removeRecipe(@Path("id") recipeId: String): Response<String>
 }
